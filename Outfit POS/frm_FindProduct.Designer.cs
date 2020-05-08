54b3ce8fded40d7438d6ed82291a31f6c0b5d043
David Sanchez Ortiz <david.sanchez@postreslacteosromar.onmicrosoft.com>
2020-05-08 14:39:51 +0200

    .

---
+++ Outfit POS/frm_FindProduct.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace Outfit_POS
+{
+    partial class frm_FindProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStocksIn = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ListView1 = new System.Windows.Forms.ListView();
+            this.ColumnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.picClose = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 87);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1164, 72);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Acción";
+            // 
+            // btnStocksIn
+            // 
+            this.btnStocksIn.BackColor = System.Drawing.Color.Transparent;
+            this.btnStocksIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStocksIn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStocksIn.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.btnStocksIn.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnStocksIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStocksIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.btnStocksIn.Location = new System.Drawing.Point(144, 110);
+            this.btnStocksIn.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStocksIn.Name = "btnStocksIn";
+            this.btnStocksIn.Size = new System.Drawing.Size(100, 34);
+            this.btnStocksIn.TabIndex = 11;
+            this.btnStocksIn.Text = "Seleccionar";
+            this.btnStocksIn.UseVisualStyleBackColor = false;
+            this.btnStocksIn.Click += new System.EventHandler(this.btnStocksIn_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Transparent;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.btnClose.Location = new System.Drawing.Point(249, 23);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 34);
+            this.btnClose.TabIndex = 10;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.btnSearch.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.btnSearch.Location = new System.Drawing.Point(32, 23);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 34);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(127, 24);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.ListView1);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1172, 769);
+            this.splitContainer1.SplitterDistance = 166;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.picClose);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, -1);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1185, 80);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 25.75F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(79, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 75);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Producto";
+            // 
+            // ListView1
+            // 
+            this.ListView1.BackColor = System.Drawing.Color.White;
+            this.ListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnHeader4,
+            this.ColumnHeader5,
+            this.ColumnHeader6,
+            this.ColumnHeader7,
+            this.ColumnHeader8,
+            this.ColumnHeader9,
+            this.ColumnHeader10,
+            this.ColumnHeader11});
+            this.ListView1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListView1.FullRowSelect = true;
+            this.ListView1.GridLines = true;
+            this.ListView1.HideSelection = false;
+            this.ListView1.Location = new System.Drawing.Point(0, 2);
+            this.ListView1.Margin = new System.Windows.Forms.Padding(4);
+            this.ListView1.Name = "ListView1";
+            this.ListView1.Size = new System.Drawing.Size(1171, 557);
+            this.ListView1.TabIndex = 30;
+            this.ListView1.UseCompatibleStateImageBehavior = false;
+            this.ListView1.View = System.Windows.Forms.View.Details;
+            this.ListView1.DoubleClick += new System.EventHandler(this.ListView1_DoubleClick);
+            // 
+            // ColumnHeader4
+            // 
+            this.ColumnHeader4.Text = "Product No. ";
+            this.ColumnHeader4.Width = 80;
+            // 
+            // ColumnHeader5
+            // 
+            this.ColumnHeader5.Text = "Item Code";
+            this.ColumnHeader5.Width = 141;
+            // 
+            // ColumnHeader6
+            // 
+            this.ColumnHeader6.Text = "Product Description";
+            this.ColumnHeader6.Width = 290;
+            // 
+            // ColumnHeader7
+            // 
+            this.ColumnHeader7.Text = "Barcode";
+            this.ColumnHeader7.Width = 106;
+            // 
+            // ColumnHeader8
+            // 
+            this.ColumnHeader8.Text = "Category";
+            this.ColumnHeader8.Width = 110;
+            // 
+            // ColumnHeader9
+            // 
+            this.ColumnHeader9.Text = "Unit Price";
+            this.ColumnHeader9.Width = 98;
+            // 
+            // ColumnHeader10
+            // 
+            this.ColumnHeader10.Text = "Stocks on Hand";
+            this.ColumnHeader10.Width = 95;
+            // 
+            // ColumnHeader11
+            // 
+            this.ColumnHeader11.Text = "Reorder Level";
+            this.ColumnHeader11.Width = 85;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(1, 560);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1185, 38);
+            this.panel1.TabIndex = 11;
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(127, 24);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(127, 24);
+            this.addToolStripMenuItem.Text = "Add";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.refreshToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(128, 76);
+            // 
+            // picClose
+            // 
+            this.picClose.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.picClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picClose.Image = global::Outfit_POS.Properties.Resources.cerrar;
+            this.picClose.Location = new System.Drawing.Point(1135, 4);
+            this.picClose.Margin = new System.Windows.Forms.Padding(4);
+            this.picClose.Name = "picClose";
+            this.picClose.Size = new System.Drawing.Size(33, 26);
+            this.picClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picClose.TabIndex = 3;
+            this.picClose.TabStop = false;
+            this.picClose.Click += new System.EventHandler(this.picClose_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = global::Outfit_POS.Properties.Resources.productos1;
+            this.pictureBox2.Location = new System.Drawing.Point(11, 6);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 54);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // frm_FindProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 769);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnStocksIn);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frm_FindProduct";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frm_FindProduct_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox picClose;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        internal System.Windows.Forms.ListView ListView1;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader4;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader5;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader6;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader7;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader8;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader9;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader10;
+        internal System.Windows.Forms.ColumnHeader ColumnHeader11;
+        private System.Windows.Forms.Button btnStocksIn;
+    }
+    }
---
+++ Outfit POS/frm_FindProduct.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+
+namespace Outfit_POS
+{
+    public partial class frm_FindProduct : Form
+    {
+        public int productID { get; set; }
+        public frm_FindProduct()
+        {
+            InitializeComponent();
+        }
+        public void LoadProducts(string strSearch)
+        {
+            try
+            {
+                SQLConn.sqL = "SELECT ProductNo, ProductCOde, P.Description, Barcode, UnitPrice, StocksOnHand, ReorderLevel, CategoryName FROM Product as P LEFT JOIN Category C ON P.CategoryNo = C.CategoryNo WHERE P.Description LIKE '" + strSearch + "%' ORDER BY Description";
+                SQLConn.ConnDB();
+                SQLConn.cmd = new MySqlCommand(SQLConn.sqL, SQLConn.conn);
+                SQLConn.dr = SQLConn.cmd.ExecuteReader();
+
+                ListViewItem x = null;
+                ListView1.Items.Clear();
+
+
+                while (SQLConn.dr.Read() == true)
+                {
+                    x = new ListViewItem(SQLConn.dr["ProductNo"].ToString());
+                    x.SubItems.Add(SQLConn.dr["ProductCode"].ToString());
+                    x.SubItems.Add(SQLConn.dr["Description"].ToString());
+                    x.SubItems.Add(SQLConn.dr["Barcode"].ToString());
+                    x.SubItems.Add(SQLConn.dr["CategoryName"].ToString());
+                    x.SubItems.Add(Strings.Format(SQLConn.dr["UnitPrice"], "#,##0.00"));
+                    x.SubItems.Add(SQLConn.dr["StocksOnHand"].ToString());
+                    x.SubItems.Add(SQLConn.dr["ReOrderLevel"].ToString());
+
+                    ListView1.Items.Add(x);
+                }
+            }
+            catch (Exception ex)
+            {
+                Interaction.MsgBox(ex.ToString());
+            }
+            finally
+            {
+                SQLConn.cmd.Dispose();
+                SQLConn.conn.Close();
+            }
+        }
+
+        private void frm_FindProduct_Load(object sender, EventArgs e)
+        {
+            LoadProducts("");
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            SQLConn.strSearch = InputBox.Show("ENTER PRODUCT NAME.", "Search Product", " ").Text;
+
+            if (SQLConn.strSearch.Length >= 1)
+            {
+                LoadProducts(SQLConn.strSearch.Trim());
+            }
+            else if (string.IsNullOrEmpty(SQLConn.strSearch))
+            {
+                return;
+            }
+        }
+
+        private void btnStocksIn_Click(object sender, EventArgs e)
+        {
+            if (ListView1.Items.Count == 0)
+            {
+                Interaction.MsgBox("Please select record to add stock", MsgBoxStyle.Exclamation, "StocksIn");
+                return;
+            }
+            try
+            {
+                if (string.IsNullOrEmpty(ListView1.FocusedItem.Text))
+                {
+
+                }
+                else
+                {
+
+                    productID = Convert.ToInt32(ListView1.FocusedItem.Text);
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
+            catch
+            {
+                Interaction.MsgBox("Please select record to add stock", MsgBoxStyle.Exclamation, "StocksIn");
+                return;
+            }
+        }
+
+        private void ListView1_DoubleClick(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                if (string.IsNullOrEmpty(ListView1.SelectedItems[0].SubItems[0].Text))
+                {
+
+                }
+                else
+                {
+
+                    productID = Convert.ToInt32(ListView1.SelectedItems[0].SubItems[0].Text);
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
+            catch
+            {
+                Interaction.MsgBox("Please select record to add stock", MsgBoxStyle.Exclamation, "StocksIn");
+                return;
+            }
+        }
+    }
+}
